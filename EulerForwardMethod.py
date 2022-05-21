93e29d401b0bff2076cdf304f71fdd475dad4de0
gbeltranl <g.beltranl@uniandes.edu.co>
2022-05-20 19:53:32 -0500

    EulerForward funcionando

--- EulerForwardMethod.py
+++ EulerForwardMethod.py
@@ -2,42 +2,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = 0.1
+a = 0.02
 b = 0.2
-c = -65
+c = -50
 d = 2
-i = 14
+_I = 10
 
 
-def ecuacion1(v, u):
-    return 0.04 * (v ** 2) + 4 * v + 140 - u + i
+def ecuacion1(v, u, i):
+    return (0.04 * (v**2)) + (5*v)+ 140 - u + i
 
 
 def ecuacion2(v, u):
     return a * ((b * v) - u)
 
-
 def eulerForward(t0, tf, V0, U0):
-    h = 0.01
+    h = 0.5
     x = np.arange(t0, tf + h, h)
     V = np.zeros(len(x))
     V[0] = V0
     U = np.zeros(len(x))
     U[0] = U0
+    I = np.zeros(len(x))
+    I[200:1500] = _I
     for iter in range(1, len(x)):
         if (V[iter - 1] >= 30):
             V[iter] = c
             U[iter] = U[iter - 1] + d
-            continue
-        V[iter] = V[iter - 1] + h * ecuacion1(V[iter - 1], U[iter - 1])
-        U[iter] = U[iter - 1] + h * ecuacion2(V[iter - 1], U[iter - 1])
-    # Graficamos la estimación de Y(t)obtenida
+            V[iter-1] = 30
+        else:
+            V[iter] = V[iter - 1] + h * ecuacion1(V[iter - 1], U[iter - 1], I[iter-1])
+            U[iter] = U[iter - 1] + h * ecuacion2(V[iter - 1], U[iter - 1])
     plt.figure()
-    plt.plot(x, U, "r")
-    plt.show()
-    plt.figure()
-    plt.plot(x, V, "r")
-    plt.grid(1)
+    # plot lines
+    plt.plot(x, V, label="V")[0]
+    plt.plot(x, U, label="U")[0]
+    plt.plot(x, I, label="I")[0]
+    plt.legend(loc="upper right")
     plt.show()
 
-eulerForward(0, 20, -65, -14)
+eulerForward(0, 1000, -70, -14)
