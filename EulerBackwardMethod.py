24a809a925e1a4ac54cea700032448fe3eec668c
gbeltranl <g.beltranl@uniandes.edu.co>
2022-05-22 13:15:41 -0500

    EulerBackward funcionando

--- EulerBackwardMethod.py
+++ EulerBackwardMethod.py
@@ -1,8 +1,9 @@
 ##
 import numpy as np
 import matplotlib.pyplot as plt
+
 t0 = 0
-tf= 1000
+tf = 1000
 a = 0.02
 b = 0.2
 c = -50
@@ -10,6 +11,13 @@ d = 2
 _I = 10
 h = 0.5
 x = np.arange(t0, tf + h, h)
+
+
+def IValues(_I):
+    I = np.zeros(len(x))
+    I[200:1500] = _I
+    return I
+
 
 def ecuacion1(v, u, i):
     return (0.04 * (v ** 2)) + (5 * v) + 140 - u + i
@@ -24,8 +32,7 @@ def eulerForward(V0, U0):
     V[0] = V0
     U = np.zeros(len(x))
     U[0] = U0
-    I = np.zeros(len(x))
-    I[200:1500] = _I
+    I = IValues(_I)
     for iter in range(1, len(x)):
         if (V[iter - 1] >= 30):
             V[iter] = c
@@ -34,10 +41,29 @@ def eulerForward(V0, U0):
         else:
             V[iter] = V[iter - 1] + h * ecuacion1(V[iter - 1], U[iter - 1], I[iter - 1])
             U[iter] = U[iter - 1] + h * ecuacion2(V[iter - 1], U[iter - 1])
-    return V, U, I
+    return V, U
 
 
-V, U, I= eulerForward(-70, -14)
+def eulerBackward(V0, U0, VFor, UFor):
+    V = np.zeros(len(x))
+    V[0] = V0
+    U = np.zeros(len(x))
+    U[0] = U0
+    I = IValues(_I)
+    for iter in range(1, len(x)):
+        if (V[iter - 1] >= 30):
+            V[iter] = c
+            U[iter] = U[iter - 1] + d
+            V[iter - 1] = 30
+        else:
+            V[iter] = V[iter - 1] + h * ecuacion1(VFor[iter], UFor[iter], I[iter])
+            U[iter] = U[iter - 1] + h * ecuacion2(VFor[iter], UFor[iter])
+    return V, U
+
+
+I = IValues(_I)
+VFor, UFor = eulerForward(-70, -14)
+V, U = eulerBackward(-70, -14, VFor, UFor)
 plt.figure()
 # plot lines
 plt.plot(x, V, label="V")[0]
@@ -47,4 +73,3 @@ plt.legend(loc="upper right")
 plt.show()
 
 ##
-
