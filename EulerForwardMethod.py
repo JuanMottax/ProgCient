6c7437a7d044a20e51be53f681a8a84b4339f65d
gbeltranl <g.beltranl@uniandes.edu.co>
2022-05-22 13:16:58 -0500

    EulerBackward funcionando

--- EulerForwardMethod.py
+++ EulerForwardMethod.py
@@ -1,8 +1,9 @@
 ##
 import numpy as np
 import matplotlib.pyplot as plt
+
 t0 = 0
-tf= 1000
+tf = 1000
 a = 0.02
 b = 0.2
 c = -50
@@ -10,6 +11,13 @@ d = 2
 _I = 10
 h = 0.5
 x = np.arange(t0, tf + h, h)
+
+
+def IValues(_I):
+    I = np.zeros(len(x))
+    I[200:1500] = _I
+    return I
+
 
 def ecuacion1(v, u, i):
     return (0.04 * (v ** 2)) + (5 * v) + 140 - u + i
@@ -25,7 +33,7 @@ def eulerForward(V0, U0):
     U = np.zeros(len(x))
     U[0] = U0
     I = np.zeros(len(x))
-    I[200:1500] = _I
+    I = IValues(_I)
     for iter in range(1, len(x)):
         if (V[iter - 1] >= 30):
             V[iter] = c
@@ -34,10 +42,11 @@ def eulerForward(V0, U0):
         else:
             V[iter] = V[iter - 1] + h * ecuacion1(V[iter - 1], U[iter - 1], I[iter - 1])
             U[iter] = U[iter - 1] + h * ecuacion2(V[iter - 1], U[iter - 1])
-    return V, U, I
+    return V, U
 
 
-V, U, I= eulerForward(-70, -14)
+I = IValues(_I)
+V, U = eulerForward(-70, -14)
 plt.figure()
 # plot lines
 plt.plot(x, V, label="V")[0]
@@ -47,4 +56,3 @@ plt.legend(loc="upper right")
 plt.show()
 
 ##
-
